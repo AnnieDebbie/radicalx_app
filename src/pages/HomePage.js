815bbeb7774c793512de34799a0ac8f2c797ceb8
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-11-30 15:18:02 +0100

    Add insights section to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,22 @@
-import classes from "./HomePage.module.css"
-import Header from "../components/header/Header"
-import SideBar from "../components/side_bar/SideBar"
+import classes from "./HomePage.module.css";
+import Header from "../components/header/Header";
+import SideBar from "../components/side_bar/SideBar";
+import ProgressChart from "../components/Insights/ProgressChart";
+import { dummy_data } from "../components/Insights/StatisticsData";
+import InsightsSection from "../components/Insights/InsightsSection";
 
-const HomePage=()=>{
-return <div className={classes["homepage-container"]}>
-    <SideBar/>
-    <Header/>
-</div>
-}
+const HomePage = () => {
+  return (
+    <div className={classes["homepage-container"]}>
+      <SideBar />
 
-export default HomePage
+      <div className={classes["not-side"]}>
+      <Header />
+      <InsightsSection />
+
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
