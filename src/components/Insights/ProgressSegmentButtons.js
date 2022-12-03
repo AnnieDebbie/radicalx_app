3f30f7b2b7ed23a0dd0f87d9fa10617f6801f79f
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-12-03 14:02:49 +0100

    Create and style segment component

--- src/components/Insights/ProgressSegmentButtons.js
+++ src/components/Insights/ProgressSegmentButtons.js
@@ -3,24 +3,33 @@ import "./Segment.css";
 
 const ProgressSegmentButtons = (props) => {
   const defaultIdx = 0;
+  const segments = ["this week", "this month"];
   const [activeIndex, setActiveIndex] = useState(defaultIdx);
-  const segments = ["this week", "this month", "select dates"];
+  // const [selectedContent, setSelectedContent]=useState()
+
   const onSegmentClick = (idx) => {
     setActiveIndex(idx);
     console.log(idx);
   };
   return (
-    <div>
+    <div className="segment-container">
       {segments.map((item, idx) => (
         <button
           key={idx}
           onClick={() => onSegmentClick(idx)}
-          className={idx === activeIndex ? "active" : "inactive"}
+          className={`segment-buttons ${
+            idx === activeIndex ? "active" : "inactive"
+          }`}
         >
           {" "}
           {item}{" "}
         </button>
       ))}
+
+      <button className="container">
+        <span>Select dates</span>
+        <input className="date" type="date" id="" />
+      </button>
     </div>
   );
 };
