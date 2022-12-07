897dba0418fca7e41c03641dfb7ef77c1646cdd3
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-12-07 13:43:07 +0100

    Add more rows to internship tracks table

--- src/components/internship_tracks/InternshipTracksTable.js
+++ src/components/internship_tracks/InternshipTracksTable.js
@@ -1,15 +1,18 @@
-import TracksTableHeader from "./TableHeader"
-import InternshipTracks from "./InternshipTracks"
+import TracksTableHeader from "./TableHeader";
+import InternshipTracks from "./InternshipTracks";
 
-const InternshipTracksTable=(props)=>{
-
-    return <section>
-        {/* <table>
-            {/* <TracksTableHeader/> 
-        </table> */}
-            
-        <InternshipTracks/>
+const InternshipTracksTable = (props) => {
+  return (
+    <section className="table-section">
+      <table className="table">
+        <TracksTableHeader />
+        <InternshipTracks />
+        <InternshipTracks />
+        <InternshipTracks />
+        <InternshipTracks />
+      </table>
     </section>
-}
+  );
+};
 
-export default InternshipTracksTable
+export default InternshipTracksTable;
